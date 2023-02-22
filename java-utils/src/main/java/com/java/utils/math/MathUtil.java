4037cfac523185676127f9b673ced7448f46eabe
林周 <970060731@qq.com>
2023-02-22 10:53:02 +0800

    版本调整

--- java-utils/src/main/java/com/java/utils/math/MathUtil.java
+++ java-utils/src/main/java/com/java/utils/math/MathUtil.java
@@ -89,4 +89,36 @@ public class MathUtil {
         return a - b;
     }
 
+    public static int min(int a, int b) {
+        return Math.min(a,b);
+    }
+
+
+    public static long min(long a, long b) {
+        return Math.min(a,b);
+    }
+
+    public static float min(float a, float b) {
+        return Math.min(a,b);
+    }
+    public static double min(double a, double b) {
+        return Math.min(a,b);
+    }
+
+    public static int max(int a, int b) {
+        return Math.max(a,b);
+    }
+
+    public static long max(long a, long b) {
+        return Math.max(a,b);
+    }
+    public static float max(float a, float b) {
+        return Math.max(a,b);
+    }
+
+
+    public static double max(double a, double b) {
+        return Math.max(a,b);
+    }
+
 }
